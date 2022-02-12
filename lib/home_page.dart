45fd86e7740c3adc75116cfc50101d6b6c732bb2
Sanjay Kumar <sanjay@inncrewin.com>
2022-02-12 20:55:09 +0530

    learning about scaffold, appbar for header, drawer for sidebarmenu, creating Homepage component and using in another component

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  final int days = 50;
+  final String name = "sanjay";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+        body: Center(
+          child: Container(
+            child: Text("welcome to $days days tutorial by $name"),
+          ),
+        ),
+        drawer: Drawer(),
+    );
+  }
+}
