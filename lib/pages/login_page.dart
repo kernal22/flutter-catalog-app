fd445cb2dfa97aca8ff14a1a298e0710d568ad28
Sanjay Kumar <sanjay@inncrewin.com>
2022-02-12 23:58:28 +0530

    created login form, creating textfield, creating button and button property and event

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,17 +7,57 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page", 
-          style: TextStyle(
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-            fontSize: 20
-        ),
-        textScaleFactor: 1.0,
-        ),
-      ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login_image.png",
+            fit: BoxFit.cover
+           ),
+           SizedBox(
+             height: 20.0,
+             child: Text("User"),
+           ),
+           Text(
+              "Welcome", 
+              style: TextStyle(
+                fontSize: 22, 
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+              child: Column(children: [
+              TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Enter Username', 
+                labelText: 'Username'
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Enter Password', 
+                  labelText: 'Password',
+                  ),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                child: Text('Login'),
+                style: TextButton.styleFrom(),
+                onPressed: () {
+                  print("hi code for");
+                },)
+            ],
+          ),
+            )
+        ],
+      )
     );
   }
 }
